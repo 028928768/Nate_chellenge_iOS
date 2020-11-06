3dee011fe3e3aa83c40cf30cca8ae2f4073def67
028928768 <kantachat_2539@hotmail.com>
2020-11-06 11:36:11 +0000

    Initial Commit

---
+++ Nate_chellenge_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nate_chellenge_iOS
+//
+//  Created by Kantachat Puagkajee on 06/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
