7d0e261eb155579622a1fba19e4524efbfdadba9
028928768 <kantachat_2539@hotmail.com>
2020-11-06 16:14:04 +0000

    Home add view container and segment controller

--- Nate_chellenge_iOS/HomeViewController.swift
+++ Nate_chellenge_iOS/HomeViewController.swift
@@ -7,11 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeViewController: UIViewController {
 
+    @IBOutlet weak var PageImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+      
     }
 
 
