0ea9678957e45a69aae17c76871c64672384d7f8
028928768 <kantachat_2539@hotmail.com>
2020-11-07 00:40:44 +0000

    implemented transition library

---
+++ Nate_chellenge_iOS/FeedViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FeedViewController.swift
+//  Nate_chellenge_iOS
+//
+//  Created by Kantachat Puagkajee on 07/11/2020.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return .lightContent
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        modalPresentationCapturesStatusBarAppearance = true
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
